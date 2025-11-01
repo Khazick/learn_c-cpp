b3f50d4df8e3df3c429fc88cedff004e2c65d57b
Khazick <2766469658@qq.com>
2025-11-01 15:52:17 +0800

    c语言第二十四次练习

---
+++ 24.practice_c_2025_11_1/24.practice_c_2025_11_1/main.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+int main1(){
+	int a = 10000;
+	FILE * pf = fopen("test.txt", "wb");
+	fwrite(&a, 4, 1, pf);//⼆进制的形式写到⽂件中
+	fclose(pf);
+	pf = NULL;
+	return 0;
+}
+
+int main(){
+	FILE* pFile;
+	//打开⽂件
+	pFile = fopen("myfile.txt", "w");
+	//⽂件操作
+	if (pFile != NULL){
+		fputs("fopen example", pFile);
+		//关闭⽂件
+		fclose(pFile);
+		pFile = NULL;
+	}
+	return 0;
+}
